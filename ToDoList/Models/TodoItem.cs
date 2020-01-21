41720456210435f20c242433d3eaf604476a76d2
Daniel Rosas Mialma <dan11_daniel@hotmail.com>
2020-01-20 18:47:38 -0600

    Add project files.

---
+++ ToDoList/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class TodoItem
+    {
+        public String Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
